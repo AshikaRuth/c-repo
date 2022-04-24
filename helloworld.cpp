46dffe2a27e2a7aac9a3d3f369ecea4ecb778184
Ashika Ruth <62024330+AshikaRuth@users.noreply.github.com>
2022-04-24 11:01:03 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+ int main()
+ {
+ 
+ 	int a=12, b=10;
+ 	float f=3.5;
+ 	char c='A';
+ 	double d=3.56789;
+ 	cout <<a<<endl;
+ 	a=a+b;
+ 	cout<<a<<" "<<f
+ 	;
+ 	return 0;
+ }
