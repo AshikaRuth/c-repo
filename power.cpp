6b63030336a2fe3e7729470bc24a574cffa94f06
Ashika Ruth Saldana <62024330+AshikaRuth@users.noreply.github.com>
2022-06-06 22:26:28 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+int pow(int no,int po)
+{
+	int ans=1;
+	for(int i=0;i<po;i++)
+	{
+		ans=no*no;
+	}
+	return ans;
+}
+int main()
+{
+	int n,p,answer;
+	cout<<"Enter No:";
+	cin>>n;
+	cout<<"Enter power:";
+	cin>>p;
+	cout<<"Answer is:"<<pow(n,p)<<endl;
+	return 0;
+}
